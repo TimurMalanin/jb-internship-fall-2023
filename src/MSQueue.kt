c6b22f7c0cb1266925394dfb54734fc180decef5
Timur05032003 <67740879+Timur05032003@users.noreply.github.com>
2023-09-18 10:44:56 +0200

    MSQueue

--- src/MSQueue.kt
+++ src/MSQueue.kt
@@ -1,15 +1,47 @@
 import java.util.concurrent.atomic.AtomicReference
 
 class MSQueue<E> : Queue<E> {
-    private val head: AtomicReference<Node<E>>
-    private val tail: AtomicReference<Node<E>>
+    private val dummy = Node<E>(null)
+    private val head: AtomicReference<Node<E>> = AtomicReference(dummy)
+    private val tail: AtomicReference<Node<E>> = AtomicReference(dummy)
 
     override fun enqueue(element: E) {
-        TODO("Implement me")
+        val node = Node(element)
+        while (true) {
+            val tail = this.tail.get()
+            val next = tail.next.get()
+            if (tail == this.tail.get()) {
+                if (next == null) {
+                    if (tail.next.compareAndSet(next, node)) {
+                        break
+                    }
+                } else {
+                    this.tail.compareAndSet(tail, next)
+                }
+            }
+        }
+        this.tail.compareAndSet(tail.get(), node)
     }
 
     override fun dequeue(): E? {
-        TODO("implement me")
+        while (true) {
+            val head = this.head.get()
+            val tail = this.tail.get()
+            val next = head.next.get()
+            if (head == this.head.get()) {
+                if (head == tail) {
+                    if (next == null) {
+                        return null
+                    }
+                    this.tail.compareAndSet(tail, next)
+                } else {
+                    val value = next?.element
+                    if (this.head.compareAndSet(head, next)) {
+                        return value
+                    }
+                }
+            }
+        }
     }
 
     // FOR TEST PURPOSE, DO NOT CHANGE IT.
